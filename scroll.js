64437fe9ca4d77532209c96c23d86aaffac72eac
Pranshu Namdeo <156303738+namdpran8@users.noreply.github.com>
2024-07-21 03:09:28 +0530

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+    let currentIndex = 0;
+    const options = $(".option");
+    const interval = 2000; // Interval in milliseconds
+  
+    function showNextOption() {
+      options.removeClass("active");
+      $(options[currentIndex]).addClass("active");
+      currentIndex = (currentIndex + 1) % options.length;
+    }
+  
+    setInterval(showNextOption, interval);
+  
+    // Optionally, you can also keep the click event handler
+    options.click(function() {
+      options.removeClass("active");
+      $(this).addClass("active");
+      currentIndex = options.index(this);
+    });
+  });
+  
